dd02881de738affd4ef2b82d4ea5d4ef9955a6ec
Renan Venâncio <renan_1419@hotmail.com>
2018-08-11 13:06:45 -0300

    Insert inserido

--- index.php
+++ index.php
@@ -17,9 +17,18 @@ echo $root -> getIdProd();
 
 $lista = Produtos::getList();
 echo json_encode($lista);
-*/
+
 
 $busca = Produtos::search("LARANJA");
 echo json_encode($busca);
+*/
+$prod = new Produtos();
+$prod -> loadById(1);
+$prod -> update("ABACAXI");
+
+echo $prod;
+
+
+
 
 ?>
